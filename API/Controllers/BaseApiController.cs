0dd8aa5208548113fc6a9d7cea1d90381e22c2f9
Danjie Zhou <danjiezhou@hotmail.com>
2023-04-12 13:58:03 +0200

    End of section 2

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,14 @@
+//Initial controller will be derived from controllers
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class BaseApiController : ControllerBase
+    {
+
+    }
+}
